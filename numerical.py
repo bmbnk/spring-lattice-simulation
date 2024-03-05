02d1320d3535cd0cd7c33cdf5311dff439c12c49
Jakub Bembenek <jakub.bembenekk@gmail.com>
2024-03-05 07:32:07 +0100

    Add multiple mass points calculation

--- numerical.py
+++ numerical.py
@@ -1,21 +1,23 @@
 from physics import MassPoint
 
 
-def euler(mp: MassPoint, t, dt, a):
+def euler(mps: list[MassPoint], t, dt, a):
     steps = range(int(t / dt))
 
     for _ in steps:
-        mp.coor += mp.v * dt
-        mp.v += a() * dt
+        for mp in mps:
+            mp.coor += mp.v * dt
+            mp.v += a() * dt
 
 
-def verlet(mp: MassPoint, t, dt, a):
+def verlet(mps: list[MassPoint], t, dt, a):
     steps = range(int(t / dt))
 
     a1 = a()
     for _ in steps:
-        mp.coor += mp.v * dt + a1 * dt**2 / 2
-        a2 = a()
-        mp.v += (a2 + a1) * dt / 2
+        for mp in mps:
+            mp.coor += mp.v * dt + a1 * dt**2 / 2
+            a2 = a()
+            mp.v += (a2 + a1) * dt / 2
 
-        a1 = a2
+            a1 = a2
