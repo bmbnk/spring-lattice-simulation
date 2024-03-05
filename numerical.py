faeae64c09cf8d1e5936010fe723926561d22137
Jakub Bembenek <jakub.bembenekk@gmail.com>
2024-03-05 12:31:54 +0100

    Fix verlet method

--- numerical.py
+++ numerical.py
@@ -18,7 +18,7 @@ def verlet(mps: tuple[MassPoint], t, dt, as_: tuple):
     for _ in steps:
         for i, mp in enumerate(mps):
             mp.coor += mp.v * dt + a1[i] * dt**2 / 2
-            a2 = as_[i](mp)
-            mp.v += (a2 + a1) * dt / 2
+            a2 = as_[i]()
+            mp.v += (a2 + a1[i]) * dt / 2
 
             a1[i] = a2
